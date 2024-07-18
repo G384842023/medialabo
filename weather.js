d1ad55d2b3e877e2ca8113e3b5a9d1e406d643d9
G384842023 <238484@st.takushoku-u.ac.jp>
2024-07-18 16:47:14 +0900

    保存

--- weather.js
+++ weather.js
@@ -3,41 +3,44 @@ let b = document.querySelector('button#getWeather');
 b.addEventListener('click', showSelectResult);
 
 function showSelectResult() {
-    let cityId = document.querySelector('select#city-select').value;
-    if (cityId) {
-        console.log('都市id:' + cityId);
-        sendRequest(cityId);
-    } else {
-        alert('都市を選択してください');
-    }
+  let cityId = document.querySelector('select#city-select').value;
+  if (cityId) {
+    console.log('都市id:' + cityId);
+    sendRequest(cityId);
+  } else {
+    alert('都市を選択してください');
+  }
 }
 
 function sendRequest(cityId) {
-    // URL を設定
-    let url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${cityId}.json`;
+  // URL を設定
+  let url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${cityId}.json`;
 
-    // 通信開始
-    axios.get(url)
-        .then(showResult)   // 通信成功
-        .catch(showError)   // 通信失敗
-        .then(finish);      // 通信の最後の処理
+  // 通信開始
+  axios.get(url)
+      .then(showResult)   // 通信成功
+      .catch(showError)   // 通信失敗
+      .then(finish);      // 通信の最後の処理
 }
 
 // 通信が成功した時の処理
 function showResult(resp) {
-    // サーバから送られてきたデータを出力
-    let data = resp.data;
-    print(data); // データを表示
+  // サーバから送られてきたデータを出力
+  let data = resp.data;
+
+  // データを表示する関数を呼び出し
+  print(data);
 }
 
 // 通信エラーが発生した時の処理
 function showError(err) {
-    console.log(err);
+  console.log(err);
+  alert('天気情報を取得できませんでした。');
 }
 
 // 通信の最後にいつも実行する処理
 function finish() {
-    console.log('Ajax 通信が終わりました');
+  console.log('Ajax 通信が終わりました');
 }
 
 function print(data) {
