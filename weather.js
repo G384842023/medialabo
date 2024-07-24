066ecc78381665cb65ac27fec7731f7ba2f6efc3
G384842023 <238484@st.takushoku-u.ac.jp>
2024-07-24 12:56:04 +0900

    保存

--- weather.js
+++ weather.js
@@ -1,87 +1,73 @@
-//ボタン処理
-let b = document.querySelector('button#getWeather');
-b.addEventListener('click', showSelectResult);
+let re = document.querySelector('div#result');
+let p1 = document.createElement('li');
+let p2 = document.createElement('li');
+let p3 = document.createElement('li');
+let p4 = document.createElement('li');
+let p5 = document.createElement('li');
+let p6 = document.createElement('li');
 
-function showSelectResult() {
-  let cityId = document.querySelector('select#city-select').value;
-  if (cityId) {
-    console.log('都市id:' + cityId);
-    sendRequest(cityId);
-  } else {
-    alert('都市を選択してください');
-  }
-}
+    // ボタン処理
+    let b = document.querySelector('button#getWeather');
+    b.addEventListener('click', showSelectResult);
 
-function sendRequest(cityId) {
-  // URL を設定
-  let url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${cityId}.json`;
+    function showSelectResult() {
+        let cityId = document.querySelector('select#city-select').value;
+        if (cityId) {
+            console.log('都市id:' + cityId);
+            sendRequest(cityId);
+        } else {
+            alert('都市を選択してください');
+        }
+    }
 
-  // 通信開始
-  axios.get(url)
-      .then(showResult)   // 通信成功
-      .catch(showError)   // 通信失敗
-      .then(finish);      // 通信の最後の処理
-}
+    function sendRequest(cityId) {
+        // URL を設定
+        let url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${cityId}.json`;
 
-// 通信が成功した時の処理
-function showResult(resp) {
-  // サーバから送られてきたデータを出力
-  let data = resp.data;
+        // 通信開始
+        axios.get(url)
+            .then(showResult)   // 通信成功
+            .catch(showError)   // 通信失敗
+            .then(finish);      // 通信の最後の処理
+    }
 
-  if (typeof data === 'string') {
-    data = JSON.parse(data);
-  }
+    // 通信が成功した時の処理
+    function showResult(resp) {
+        // サーバから送られてきたデータを出力
+        let data = resp.data;
 
-  // データを表示する関数を呼び出し
-  print(data);
-}
+        if (typeof data === 'string') {
+            data = JSON.parse(data);
+        }
 
-// 通信エラーが発生した時の処理
-function showError(err) {
-  console.log(err);
-  alert('天気情報を取得できませんでした。');
-}
+        // データを表示する関数を呼び出し
+        print(data);
+    }
 
-// 通信の最後にいつも実行する処理
-function finish() {
-  console.log('Ajax 通信が終わりました');
-}
+    // 通信エラーが発生した時の処理
+    function showError(err) {
+        console.log(err);
+        alert('天気情報を取得できませんでした。エラー: ' + err.message);
+    }
 
-function print(data) {
-  const weatherInfo = document.getElementById('weather-info');
+    // 通信の最後にいつも実行する処理
+    function finish() {
+        console.log('Ajax 通信が終わりました');
+    }
 
-  // Clear previous content
-  weatherInfo.textContent = '';
+    function print(data) {
+        p1.textContent = "都市名 : " + data.name;
+        re.insertAdjacentElement('beforeend', p1);
 
-  // Create elements for each piece of information
-  const cityHeading = document.createElement('h2');
-  cityHeading.textContent = data.name;
+        p2.textContent = "天気 : " + data.weather[0].description;
+        re.insertAdjacentElement('beforeend', p2); 
 
-  const weatherDesc = document.createElement('p');
-  weatherDesc.textContent = `天気: ${data.weather[0].description}`;
+        p3.textContent = "最低気温 : " + data.main.temp_min + "℃";
+        re.insertAdjacentElement('beforeend', p3); 
 
-  const minTemp = document.createElement('p');
-  minTemp.textContent = `最低気温: ${data.main.temp_min} °C`;
+        p4.textContent = "最高気温 : " + data.main.temp_max + "℃";
+        re.insertAdjacentElement('beforeend', p4); 
 
-  const maxTemp = document.createElement('p');
-  maxTemp.textContent = `最高気温: ${data.main.temp_max} °C`;
-
-  const humidity = document.createElement('p');
-  humidity.textContent = `湿度: ${data.main.humidity} %`;
-
-  const windSpeed = document.createElement('p');
-  windSpeed.textContent = `風速: ${data.wind.speed} m/s`;
-
-  // Append elements to weatherInfo container
-  weatherInfo.appendChild(cityHeading);
-  weatherInfo.appendChild(weatherDesc);
-  weatherInfo.appendChild(minTemp);
-  weatherInfo.appendChild(maxTemp);
-  weatherInfo.appendChild(humidity);
-  weatherInfo.appendChild(windSpeed);
-}
-
-
-function clearWeatherInfo() {
-    document.getElementById('weather-info').textContent = '';
-}
+        p5.textContent = "湿度 : " + data.main.humidity + "%";
+        re.insertAdjacentElement('beforeend', p5);
+      }
